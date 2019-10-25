d4fc72a7e6a3063ab0133a9e19aedf6e99d8958c
yangcongde <211738702@qq.com>
2019-10-25 10:30:10 +0800

    测试

---
+++ 1.py
@@ -0,0 +1,16 @@
+List = ["AA","BB","CC"]
+
+print(List.pop())
+strData = "hello world"
+print(strData[6])
+
+strlist = [1,2,3,4,5,6]
+print(strlist[::-1])
+strdict = {'name':"lxl",'age':'343',"a":'b'}
+del strdict['name']
+print(strdict)
+
+data = [1,2,1,12,3,3,3,4,5,4]
+print(list(set(data)))
+
+
